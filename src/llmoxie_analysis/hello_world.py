a30fa466b14c9d0ef0b3597c88216e046280d4b7
Don Setiawan <landungs@uw.edu>
2026-09-23 18:32:18 +0000

    Add hello-world module with test

---
+++ src/llmoxie_analysis/hello_world.py
@@ -0,0 +1,8 @@
+"""Hello-world module for the llmoxie_analysis package."""
+
+from __future__ import annotations
+
+
+def hello_world() -> str:
+    """Return a friendly greeting."""
+    return "Hello, world!"
